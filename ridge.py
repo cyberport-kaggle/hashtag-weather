67f1d39bf025ebf7ecd37065d5fa155da861caca
Michael Chang <mchangun@gmail.com>
2013-11-26 21:06:41 +0800

    ridge cross validation

--- ridge.py
+++ ridge.py
@@ -2,6 +2,7 @@ import pandas as p
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import linear_model
+from IPython import embed
 
 paths = ['data/train.csv', 'data/test.csv']
 t = p.read_csv(paths[0])
@@ -14,15 +15,15 @@ X = tfidf.transform(t['tweet'])
 test = tfidf.transform(t2['tweet'])
 y = np.array(t.ix[:,4:])
 
-clf = linear_model.Ridge (alpha = .5)
+clf = linear_model.RidgeCV(alphas = np.linspace(0, 5, 10), cv = 5, normalize = True)
+# clf = linear_model.Ridge(alpha = 0.5)
 clf.fit(X, y)
 test_prediction = clf.predict(test)
 
-embed()
-
+print 'Best Alpha:', clf.alpha_
 print 'Train error: {0}'.format(np.sqrt(np.sum(np.array(np.array(clf.predict(X))-y)**2)/ (X.shape[0]*24.0)))
 
 prediction = np.array(np.hstack([np.matrix(t2['id']).T, test_prediction])) 
 col = '%i,' + '%f,'*23 + '%f'
-np.savetxt('results/ridge.csv', prediction,col, delimiter=',')
-
+np.savetxt('results/ridge.csv', prediction, col, delimiter=',')
+embed()
