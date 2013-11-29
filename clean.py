9b7a636456cb648c977c07a452b5bf47f833b5ab
Han Xu <hgcrpd@gmail.com>
2013-11-28 18:51:30 -0800

    parameterize vectorizer function

--- clean.py
+++ clean.py
@@ -2,8 +2,29 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 
 
-def get_tfidf_vectorizer(train=None):
-    tfidf = TfidfVectorizer(max_features=None, strip_accents='unicode', analyzer='word', ngram_range=(1, 3), lowercase=True, stop_words='english', min_df=5, max_df=0.5)
+def get_tfidf_vectorizer(train=None, **kwargs):
+
+    # Michael: not sure where you used these parameters, as they ended up being hard coded into the ridge_003
+    #vectorizer_args = {
+    #    'max_features': None,
+    #    'strip_accents': 'unicode',
+    #    'analyzer': 'word',
+    #    'ngram_range': (1, 3),
+    #    'lowercase': True,
+    #    'stop_words': 'english',
+    #    'min_df': 5,
+    #    'max_df': 0.5,
+    #}
+
+    vectorizer_args = {
+        'max_features': 10000,
+        'strip_accents': 'unicode',
+        'analyzer': 'word',
+    }
+
+    vectorizer_args.update(kwargs)
+
+    tfidf = TfidfVectorizer(**vectorizer_args)
     if train is not None:
         tfidf.fit(train)
     return tfidf
